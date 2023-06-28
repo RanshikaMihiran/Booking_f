4014b4e54f519deb53c4e4f273aef5217cf1a17d
RanshikaMihiran <89793493+RanshikaMihiran@users.noreply.github.com>
2023-06-28 20:02:35 +0530

    edit all

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,19 +1,18 @@
-<<<<<<< HEAD
 import Navbar from "../../components/navbar/Navbar";
-=======
 
->>>>>>> 7946752 (edit navbar)
+
+
 import Header from "../../components/navbar/header/Header";
 import "./home.css"
 
 const Home = () => {
   return (
     <div> 
-<<<<<<< HEAD
+
       <Navbar/>
-=======
+
       {/* <Navbar/> */}
->>>>>>> 7946752 (edit navbar)
+
       <Header/>
     </div>
   );
