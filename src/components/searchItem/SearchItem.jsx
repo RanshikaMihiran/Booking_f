18176b941b30599ae720ace29166174bbd390a39
RanshikaMihiran <89793493+RanshikaMihiran@users.noreply.github.com>
2023-07-03 22:02:38 +0530

    edit list

--- src/components/searchItem/SearchItem.jsx
+++ src/components/searchItem/SearchItem.jsx
@@ -11,10 +11,23 @@ const SearchItem = () => {
         <span className="siSubtitles"> Studio Apartment with Air Conditioning</span>
         <span className="siFeatures"> Entire Studio | bathroom | 21m2 full bed</span>
         <span className="siCancelOp">Free cancellation</span>
-        <span className="siCancelOpSubtitle">You can cansel later, So lock in this grwat price today</span>
+        <span className=" ">You can cansel later, So lock in this grwat price today</span>
       </div>
-      <div className="siDetails">Details</div>
+      <div className="siDetails">
+        <div className="siRating">
+          <span>Excellent</span>
+          <button>8.9</button>
+        </div>
+        <div className="siDetailsText">
+          <span className="siPrice">$100</span>
+          <span className="siTaxOp">Include taxtes and fees</span>
+          <button className="siCheckButton"> See availability</button>
+        </div>
+      </div>
+
+      
     </div>
+    
   )
 }
 
