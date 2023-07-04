01b7a22d7acb0877471c365ed9cfffe97261a12d
RanshikaMihiran <89793493+RanshikaMihiran@users.noreply.github.com>
2023-07-05 01:07:18 +0530

    client

--- src/components/navbar/featured/Featured.jsx
+++ src/components/navbar/featured/Featured.jsx
@@ -1,31 +1,40 @@
+import useFetch from "../../../hooks/useFetch"
 import "./featured.css"
 
 const Featured = () => {
+
+  const {data,loading,error} = useFetch("/hotels/countByCity?cities=nuwaraeliya,badulla,kurunegala ")
+  
+  
   return (
     <div className="featured">
-    <div className="featuredItem">
+    {loading ? (
+     "Loading please wait"
+    ):(
+       <>
+       <div className="featuredItem">
         <img src="/Images/featured/colombo.jpg" alt="" className="featuredImg" />
         <div className="featuredTitles">  
-        <h1>Colombo</h1>
-        <h2>123 properties</h2>
+        <h1>NuwaraEliya</h1>
+        <h2>{data[0]} properties</h2>
         </div>
     </div>
     <div className="featuredItem">
         <img src="/Images/featured/ella.jpg" alt="" className="featuredImg" />
         <div className="featuredTitles">
-        <h1>Ella</h1>
-        <h2>123 properties</h2>
+        <h1>Badulla</h1>
+        <h2>{data[1]} properties</h2>
         </div>
     </div>
     <div className="featuredItem">
         <img src="/Images/featured/kandy.jpg" alt="" className="featuredImg" />
         <div className="featuredTitles">
-        <h1>kandy</h1>
-        <h2>123 properties</h2>
+        <h1>Kurunegala</h1>
+        <h2>{data[2]} properties</h2>
         </div>
+    </div> </>)}
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Featured
+export default Featured ;
